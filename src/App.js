24fd067888081e842ace2f4f4c0863767b54b703
Matija Dupan <dupan.matija@gmail.com>
2021-08-24 23:36:16 +0200

    Adding forms and custom button component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
-import Header from './components/header/header.component.jsx';
+import SignInAndSignUp from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
